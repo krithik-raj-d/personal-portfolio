c20e0b8c920cea1e0c270307242ef78b87bfbcd2
Krithik983 <rajkrithik4@gmail.com>
2026-03-05 14:33:14 +0530

    feat: add ScrollStack component; integrate Lenis for smooth scrolling; update Projects and Services components with new styles and content

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import MagicBento from "./MagicBento";
+import ScrollStack, { ScrollStackItem } from "./ScrollStack";
 
 const Projects = () => {
   return (
     <div className="bg-[url(/project.jpg)] bg-no-repeat bg-cover w-full h-screen flex justify-center md:justify-between items-center p-5 md:p-20">
-      <h1 className="text-white text-4xl md:text-6xl font-bold max-w-xl uppercase">
-        Where Ideas Meet Execution
-      </h1>
-      <MagicBento
+      <div className="w-full">
+        <h1 className="text-white text-4xl md:text-6xl font-bold max-w-xl uppercase">
+          From Concept to Interface
+        </h1>
+        <p className="my-3 text-sm font-medium italic text-amber-700 bg-white/10 backdrop-blur-xs">
+          Real world applications designed with a focus on performance,
+          scalability, and intuitive user experience
+        </p>
+      </div>
+      {/* <MagicBento
         textAutoHide={true}
         enableStars={false}
         enableSpotlight
@@ -19,7 +26,22 @@ const Projects = () => {
         particleCount={0}
         glowColor="132, 0, 255"
         disableAnimations={false}
-      />
+      /> */}
+
+      <ScrollStack className="bg-transparent">
+        <ScrollStackItem itemClassName="bg-white/10 border border-white/30 backdrop-blur-sm">
+          <h2>Card 1</h2>
+          <p>This is the first card in the stack</p>
+        </ScrollStackItem>
+        <ScrollStackItem itemClassName="bg-white/10 border border-white/30 backdrop-blur-sm">
+          <h2>Card 2</h2>
+          <p>This is the second card in the stack</p>
+        </ScrollStackItem>
+        <ScrollStackItem itemClassName="bg-white/10 border border-white/30 backdrop-blur-sm">
+          <h2>Card 3</h2>
+          <p>This is the third card in the stack</p>
+        </ScrollStackItem>
+      </ScrollStack>
     </div>
   );
 };
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,9 +3,8 @@ import BlurText from "./BlurText";
 
 const Services = () => {
   return (
-    <div className="bg-[url(/service-bg.jpg)] bg-no-repeat bg-cover w-full h-screen flex flex-col md:flex-row md:justify-between items-center p-5 md:p-20">
-      {/*  */}
-      <div className=" outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs p-5 rounded-lg flex flex-col items-center gap-5">
+    <div className="bg-[url(/service-bg.jpg)] bg-no-repeat bg-cover bg-center md:bg-top w-full min-h-screen flex flex-col md:flex-row md:justify-between p-5 md:p-20">
+      <div className="outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs p-5 rounded-lg flex flex-col items-center">
         {/* <h1 className="text-white text-4xl md:text-6xl font-bold uppercase mb-5 max-w-xl">
           Where Strategy Meets Execution
         </h1> */}
@@ -22,48 +21,64 @@ const Services = () => {
           digital vision to life. 
         </p> */}
         <BlurText
-          text="I offer a comprehensive suite of services designed to bring your
-          digital vision to life."
+          text="I offer a comprehensive suite of services designed to bring your digital vision to life"
           delay={200}
           animateBy="words"
           direction="top"
           // onAnimationComplete={handleAnimationComplete}
           className="text-white text-lg md:text-xl mb-5 max-w-xl"
         />
-        <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
-          <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
-            1
-          </h1>
-          <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
-            Web Development
-          </h3>
+        <div className="w-full flex flex-col gap-7 pt-5 pb-10">
+          <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
+            <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
+              1
+            </h1>
+            <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
+              Web Development
+            </h3>
+          </div>
+          <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
+            <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
+              2
+            </h1>
+            <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
+              Mobile App Development
+            </h3>
+          </div>
+          <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
+            <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
+              3
+            </h1>
+            <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
+              Website Design & UI/UX
+            </h3>
+          </div>
+          <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
+            <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
+              4
+            </h1>
+            <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
+              SEO Optimization
+            </h3>
+          </div>
         </div>
-        <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
-          <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
-            2
-          </h1>
-          <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
-            Mobile App Development
-          </h3>
-        </div>
-        <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
-          <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
-            3
-          </h1>
-          <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
-            Website Design & UI/UX
-          </h3>
-        </div>
-        <div className="relative outline-1 outline-white/30 outline-offset-4 bg-white/10 backdrop-blur-xs px-1 py-6 rounded-lg flex items-center w-full">
-          <h1 className="absolute text-white text-4xl md:text-8xl font-bold max-w-xl uppercase ml-2">
-            4
-          </h1>
-          <h3 className="text-white text-xl md:text-2xl font-semibold ml-20 uppercase">
-            SEO Optimization
-          </h3>
-        </div>
+        <button
+          className="rounded-full w-full text-sm md:text-lg font-semibold
+             bg-white/10
+             backdrop-blur-xs
+             border border-white/30
+             text-white
+             p-5
+             cursor-pointer
+             transition-all duration-300 ease-out
+             hover:border-x-amber-600
+             hover:shadow-[0_0_15px_rgba(235,70,15,0.6)]
+             hover:scale-105 uppercase"
+        >
+          Let's work together
+        </button>
       </div>
-      <div className="relative hidden md:block">
+      <div className="relative hidden xl:block">
         <CardSwap
           cardDistance={60}
           verticalDistance={70}
